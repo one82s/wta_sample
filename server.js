39612945b75ce0cf54adaa2411a452db1f9869af
Juanito Ortega <one82s@yahoo.com>
2025-02-23 14:54:15 +0800

    Added node library for custom error handling and formatting

--- server.js
+++ server.js
@@ -2,14 +2,25 @@ require("dotenv").config();
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const responser = require("responser").default;
 const db = require("./lib/db");
-const {passport, app} = require("./lib/auth");
+const { passport, app } = require("./lib/auth");
 
 const PORT = 3000;
 
 app.use(bodyParser.json());
 
 app.use(express.static(__dirname));
+
+app.use(responser);
+
+// Used for centralized error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  const statusCode = err.status || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({ error: message });
+});
 
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
@@ -33,10 +44,10 @@ app.get(
 );
 
 app.get("/logout", (req, res) => {
-    req.logout(() => {
-      res.redirect("/");
-    });
+  req.logout(() => {
+    res.redirect("/");
   });
+});
 
 app.get("/index", async (request, response) => {
   console.log("Connected to Web App");
@@ -57,6 +68,7 @@ app.post("/names", async (request, response) => {
   const nameFromRequest = request.body.name;
   const external_API_URL = "https://api.genderize.io?name=";
   let data = "{}";
+  //Handling asynchronous errors
   try {
     // Send a GET request to the API
     const response = await fetch(external_API_URL + nameFromRequest);
@@ -83,7 +95,7 @@ app.post("/names", async (request, response) => {
   });
 
   const newItem = await name.save();
-  response.status(201).json({ scuccess: true });
+  response.send_created("Item created successfully", newItem.name)
 });
 
 // Get id that will be updated
@@ -92,13 +104,20 @@ app.get("/name/:id", async (request, response) => {
   const id = request.params.id;
   console.log("Name ID is: ", id);
   const name = await db.nameSchema.findById(id);
-  response.status(200).json(name);
+  response.send_ok(true, "ID ", name._id, "retreived successfully");
 });
 
 // Updated data from the ID that will be passed from UI
 app.put("/name/:id", async (request, response) => {
   console.log("here in put name ");
   const nameId = request.params.id;
+  // adding validation and handling specific errors
+  if (!nameId) {
+    const error = new Error("Name ID  is required");
+    error.status = 400;
+    next(error);
+  }
+
   console.log("Name ID: ", nameId);
   // Fetch the user from the database
   const nameModel = await db.nameSchema.findById(nameId);
@@ -107,7 +126,7 @@ app.put("/name/:id", async (request, response) => {
   nameModel.count = request.body.count;
   nameModel.probability = request.body.probability;
   const updatedItem = await nameModel.save();
-  response.status(200).json(updatedItem);
+  response.send_ok(true, "Item updated successfully")
 });
 
 // Delete name based on the ID passed from the UI
@@ -117,5 +136,9 @@ app.delete("/name/:id", async (request, response) => {
   // Fetch the user from the database
   const nameModel = await db.nameSchema.findById(nameId);
   await nameModel.deleteOne();
-  response.status(200).json({ message: "Deleted item" });
+  response.send_ok(true, "Item deleted successfully")
+});
+
+app.use((req, res, next) => {
+  res.send_badRequest("Endpoint not defined");
 });
