249673a2b56633e957b0644b5f63002f044fba4b
Juanito Ortega <one82s@yahoo.com>
2025-02-23 13:58:53 +0800

    Abstracted authentication

--- server.js
+++ server.js
@@ -1,106 +1,121 @@
 require("dotenv").config();
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const db = require('./lib/db')
+const express = require("express");
+const bodyParser = require("body-parser");
+const db = require("./lib/db");
+const {passport, app} = require("./lib/auth");
 
-const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
 
 app.use(express.static(__dirname));
 
-
-app.listen(PORT, ()=>{console.log('Server is running on port', PORT);});
-	
-
-
-app.get('/', async (request, response) => {
-    console.log('Connected to Web App');
-    response.sendFile(__dirname + '/names.html');
+app.listen(PORT, () => {
+  console.log("Server is running on port", PORT);
 });
 
+app.get("/", (req, res) => {
+  res.send("<a href='/auth/google'>Login with Google</a>");
+});
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => {
+    res.redirect("/index");
+  }
+);
+
+app.get("/logout", (req, res) => {
+    req.logout(() => {
+      res.redirect("/");
+    });
+  });
+
+app.get("/index", async (request, response) => {
+  console.log("Connected to Web App");
+  response.sendFile(__dirname + "/names.html");
+});
 
 // Get list of names from MongoDB
-app.get('/names', async (request, response) => {
-    console.log('here in get names');
-    const names = await db.nameSchema.find();
-    response.status(200).json(names);
+app.get("/names", async (request, response) => {
+  console.log("here in get names");
+  const names = await db.nameSchema.find();
+  response.status(200).json(names);
 });
 
 // Fetching the data to insert from External API
-app.post('/names', async (request, response) => {
-    console.log('here in create user');
-    
-    
-    const nameFromRequest = request.body.name
-    const external_API_URL = 'https://api.genderize.io?name='
-    let data = '{}';
-    try {
-        // Send a GET request to the API
-        const response = await fetch(external_API_URL+nameFromRequest);
-    
-    
-        // Check if the request was successful
-        if (!response.ok) {
-          throw new Error(`Error: ${response.status}`);
-        }
-    
-    
-        // Parse the JSON response
-        data = await response.json();
-    
-    
-        // Log the data for now (we will display it later)
-        console.log(data);
-    } catch (error) {
-        console.error('Error fetching data:', error.message);
+app.post("/names", async (request, response) => {
+  console.log("here in create user");
+
+  const nameFromRequest = request.body.name;
+  const external_API_URL = "https://api.genderize.io?name=";
+  let data = "{}";
+  try {
+    // Send a GET request to the API
+    const response = await fetch(external_API_URL + nameFromRequest);
+
+    // Check if the request was successful
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
     }
-    const Names = db.nameSchema;
-    const name = new Names({
-        name : data.name,
-        gender : data.gender,
-        count : data.count,
-        probability : data.probability,
-    });
-    
-    const newItem = await name.save();
-    response.status(201).json({scuccess:true});
+
+    // Parse the JSON response
+    data = await response.json();
+
+    // Log the data for now (we will display it later)
+    console.log(data);
+  } catch (error) {
+    console.error("Error fetching data:", error.message);
+  }
+  const Names = db.nameSchema;
+  const name = new Names({
+    name: data.name,
+    gender: data.gender,
+    count: data.count,
+    probability: data.probability,
+  });
+
+  const newItem = await name.save();
+  response.status(201).json({ scuccess: true });
 });
 
-
 // Get id that will be updated
-app.get('/name/:id', async (request, response) => {
-    console.log('here in get name ');
-    const id = request.params.id;
-    console.log('Name ID is: ', id);
-    const name = await db.nameSchema.findById(id);
-    response.status(200).json(name);
+app.get("/name/:id", async (request, response) => {
+  console.log("here in get name ");
+  const id = request.params.id;
+  console.log("Name ID is: ", id);
+  const name = await db.nameSchema.findById(id);
+  response.status(200).json(name);
 });
 
 // Updated data from the ID that will be passed from UI
-app.put('/name/:id', async (request, response) => {
-    console.log('here in put name ');
-    const nameId = request.params.id;
-    console.log('Name ID: ', nameId);
-    // Fetch the user from the database
-    const nameModel = await db.nameSchema.findById(nameId);
-    nameModel.name = request.body.name;
-    nameModel.gender = request.body.gender;
-    nameModel.count = request.body.count;
-    nameModel.probability = request.body.probability;
-    const updatedItem = await nameModel.save();
-    response.status(200).json(updatedItem);
+app.put("/name/:id", async (request, response) => {
+  console.log("here in put name ");
+  const nameId = request.params.id;
+  console.log("Name ID: ", nameId);
+  // Fetch the user from the database
+  const nameModel = await db.nameSchema.findById(nameId);
+  nameModel.name = request.body.name;
+  nameModel.gender = request.body.gender;
+  nameModel.count = request.body.count;
+  nameModel.probability = request.body.probability;
+  const updatedItem = await nameModel.save();
+  response.status(200).json(updatedItem);
 });
 
 // Delete name based on the ID passed from the UI
-app.delete('/name/:id', async (request, response) => {
-    console.log('here in delete name ');
-    const nameId = request.params.id;
-    // Fetch the user from the database
-    const nameModel = await db.nameSchema.findById(nameId);
-    await nameModel.deleteOne();
-    response.status(200).json({ message : 'Deleted item' });
+app.delete("/name/:id", async (request, response) => {
+  console.log("here in delete name ");
+  const nameId = request.params.id;
+  // Fetch the user from the database
+  const nameModel = await db.nameSchema.findById(nameId);
+  await nameModel.deleteOne();
+  response.status(200).json({ message: "Deleted item" });
 });
-
